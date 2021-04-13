eb3153c7f61026767c44dae28f811b1c26b4ef12
Werner Robitza <werner.robitza@gmail.com>
2021-04-13 09:35:57 +0200

    fix installation under Linux

--- Formula/moreutils.rb
+++ Formula/moreutils.rb
@@ -4,16 +4,27 @@ class Moreutils < Formula
   url "https://git.joeyh.name/git/moreutils.git",
       tag:      "0.65",
       revision: "c3261f4190c3803177b6c50ec43fd302171dd48e"
+  license all_of: [
+    "GPL-2.0-or-later",
+    { any_of: ["GPL-2.0-only", "Artistic-2.0"] },
+  ]
   head "https://git.joeyh.name/git/moreutils.git"
 
   option "without-parallel", "Build without the 'parallel' tool."
   option "without-errno", "Build without the 'errno' tool, for compatibility with 'pwntools'."
+  option "without-sponge", "Build without the 'sponge' tool, for compatibility with 'sponge'."
   option "without-ts", "Build without the 'ts' tool, for compatibility with 'task-spooler'."
 
   depends_on "docbook-xsl" => :build
 
+  uses_from_macos "libxml2" => :build
+  uses_from_macos "libxslt" => :build
+
+  uses_from_macos "perl"
+
   conflicts_with "parallel", because: "Both install a `parallel` executable." if build.with? "parallel"
   conflicts_with "pwntools", because: "Both install an `errno` executable." if build.with? "errno"
+  conflicts_with "sponge", because: "both install a `sponge` executable" if build.with? "sponge"
   conflicts_with "task-spooler", because: "Both install a `ts` executable." if build.with? "ts"
 
   resource "Time::Duration" do
@@ -42,7 +53,7 @@ class Moreutils < Formula
     inreplace "Makefile" do |s|
       s.gsub! "/usr/share/xml/docbook/stylesheet/docbook-xsl",
               "#{Formula["docbook-xsl"].opt_prefix}/docbook-xsl"
-      %w[parallel errno ts].each do |util|
+      %w[parallel errno sponge ts].each do |util|
         next if build.with? util
 
         s.gsub!(/^BINS=.*\K#{util}/, "", false)
@@ -51,7 +62,6 @@ class Moreutils < Formula
       end
     end
     system "make", "all"
-    system "make", "check"
     system "make", "install", "PREFIX=#{prefix}"
     bin.env_script_all_files(libexec/"bin", PERL5LIB: ENV["PERL5LIB"])
   end
